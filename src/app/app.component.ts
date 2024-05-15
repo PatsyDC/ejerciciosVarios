13bc599abfacb274fe688f5f7a1dd38128e592ae
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-05-15 16:48:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sesion10';
+  color: string = 'yellow';
+  todos: string[] = ['hola', 'chau', 'patsy'];
+  newTodo: string = '';
+
+  onchangeColor(color: string) {
+    console.log('El color seleccionado es: ', color);
+    this.color = color;
+  }
+
+  onchangeInput(){
+    this.todos.push(this.newTodo); // Agregar el nuevo todo al array
+    this.newTodo = '';
+  }
+
 }
